83a2bb76e746d55b8a3cb6eaa3482ea35f9f0fdf
ashutoshbr <ashutoshbr@proton.me>
2023-09-17 18:03:29 +0545

    v1: matrix gen without headers

---
+++ main.py
@@ -0,0 +1,27 @@
+import csv
+import numpy as np
+import pandas as pd
+
+with open("./grocery_dataset.csv", "r") as file:
+    file_contents = csv.reader(file)
+    num_of_rows = len(file.readlines())
+    file.seek(0)
+    columns = []
+    for row in file_contents:
+        for elem in row:
+            columns.append(elem)
+    # python sets are unordered but dict is ordered
+    columns = list(dict.fromkeys(columns))
+    num_of_columns = len(columns)
+
+    transactional_matrix = np.zeros((num_of_rows, num_of_columns), dtype="b")
+    file.seek(0)
+    for i, row in enumerate(file_contents):
+        for elem in row:
+            if elem in columns:
+                j = columns.index(elem)
+                transactional_matrix[i][j] = 1
+
+    df = pd.DataFrame(transactional_matrix)
+    df.to_csv("transactional_matrix.csv", index=False, header=False)
+    print(df)
